ee4fecb844dac6d70271c24c2b1b7e546efd02e3
Ye Jia-wei <ye.jia.wei@outlook.com>
2019-05-13 22:41:18 +0800

    quicksave

--- mail1/import_mails.php
+++ mail1/import_mails.php
@@ -7,16 +7,18 @@ include_once "class/MailDBAgent.php";
  */
 $mode    = $argv[1];
 $path    = $argv[2];
+//$pattern = '/.eml$/';
 $pattern = '/^[0-9]{2,}$/';
 $mail_db = new MailDBAgent("work5", "jarvis", "localhost", "27050888");
-$table   = "mails2";
-//$pattern = '/.eml$/';
+$table   = "mails";
+
 
 /*
  * 2. fetch
  */
 $output = array();
-find_files($path, $pattern, $output);
+parse_dir($path, $pattern, $output);
+
 
 /*
  * 3. output
@@ -35,26 +37,26 @@ return;
 /*
  * functions overview
  * ------------------------------------
- * find_files($path, $pattern, &$output)
- *  |- fetch_info($file, &$output)
- * 		|- find_feature($header)
- * 		|- get_subject($subject, $charset)
+ * parse_dir($path, $pattern, &$output)
+ *  |- collect_info($file, &$output)
+ * 		|- verify_epaper($header)
+ * 		|- decode($subject, $charset)
  * 		|- get_charset($mail)
  */
-function find_files($path, $pattern, &$output)
+function parse_dir($path, $pattern, &$output)
 {
 	if (is_dir($path) == false) {
 		if (preg_match($pattern, basename($path)))
-			fetch_info($path, $output);
+			collect_info($path, $output);
 	} elseif ($handle = opendir($path)) {
 		while (($entry = readdir($handle)) !== false)
 			if (substr($entry, 0, 1) != ".")
-				find_files("$path/$entry", $pattern, $output);
+				parse_dir("$path/$entry", $pattern, $output);
 		closedir($handle);
 	}
 }
 
-function fetch_info($file, &$output)
+function collect_info($file, &$output)
 {
 	echo "$file: ";
 	$mail = parse_mail($file);
@@ -72,9 +74,9 @@ function fetch_info($file, &$output)
 		if (substr($m_id,0,1) == "<" and substr($m_id,-1) == ">")
 			$m_id = substr($m_id, 1, -1);
 
-		$epaper = find_feature($mail["header"]);
+		$epaper = verify_epaper($mail["header"]);
 		$is_epaper = (empty($epaper))? 0: 1;
-		$subject = get_subject($mail["header"]["subject"][0], get_charset($mail));
+		$subject = decode($mail["header"]["subject"][0], get_charset($mail));
 		$output[] = array(
 			"user-agent" => $ua,
 			"ua-value" => $ua_value,
@@ -91,7 +93,7 @@ function fetch_info($file, &$output)
 	return $output;
 }
 
-function find_feature($header)
+function verify_epaper($header)
 {
 	if (array_key_exists("list-unsubscribe", $header))
 		return "List-Unsubscribe:";
@@ -103,7 +105,7 @@ function find_feature($header)
 	return null;
 }
 
-function get_subject($subject, $charset)
+function decode($subject, $charset)
 {
 	if (substr($subject, 0, 1) == "=") {
 		if (substr($subject, 2, 6) == "gb2312")
