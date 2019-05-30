fdb4e65edf6e3467387eb7f8c828fe7cf393e3be
Ye Jia-wei <ye.jia.wei@outlook.com>
2019-05-30 18:16:34 +0800

    quicksave

--- mail2/mid_analyzer.php
+++ mail2/mid_analyzer.php
@@ -5,11 +5,23 @@ $table   = "mails";
 
 $array = $mail_db->fetch_all("select * from `$table` order by `ua-value`");
 
+/*
 echo $array[0]["ua-value"] . "\n";
 for ($i = 1; $i < count($array); $i++) {
-	if ($array[$i]["ua-value"] != $array[$i - 1]["ua-value"])
+	if ($array[$i]["ua-value"] != $array[$i - 1]["ua-value"]) {
 		echo "\n\n" . $array[$i]["ua-value"] . "\n";
+	}
 	$value = trim($array[$i]["message-id"], "<>");
 	echo substr($value, 0, strpos($value, '@')) . "\n";
 }
+ */
+
+$count = 1;
+for ($i = 1; $i < count($array); $i++) {
+	if ($array[$i]["ua-value"] != $array[$i - 1]["ua-value"]) {
+		$count++;
+	}
+}
+
+echo $count;
 
